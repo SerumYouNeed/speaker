a5b1036342eb70e28a6959ef7be4aa1dd5f5e688
SerumYouNeed <greg.miler@gmail.com>
2026-04-23 19:46:54 +0200

    client 1.0e

--- temp.c
+++ temp.c
@@ -1,37 +1,8 @@
 #include <stdio.h>
 
-void add1(int a)
-{
-    a++;
-}
-
-struct car
-{
-    int year;
-    int price;
-    char *name;
-};
-
-void byref(struct car *c, int price)
-{
-    c->price = price;
-}
-
 int main(void)
 {
-    struct car mazda = {.year=2008, .name="mazda 6"};
-
-    struct car *ptr = &mazda;
     
-    printf("int: %zu\n", sizeof (int));
-    printf("mazda: %zu\n", sizeof mazda);
-    printf("mazda string: %zu\n", sizeof ptr->name);
-    printf("amodel: %s\n", mazda.name);
-    printf("year: %d\n", ptr->year);
 
-    byref(ptr, 20000);
-
-    printf("cena zmiana ptr: %d\n", ptr->price);
-        
     return (0);
 }
