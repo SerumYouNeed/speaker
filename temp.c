be0ac1a5a7ba860fca89e27b3f25148c8388cb02
SerumYouNeed <greg.miler@gmail.com>
2026-04-24 18:01:59 +0200

    client 1.0f

--- temp.c
+++ temp.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int myStrLen(char *s)
+{
+    char *p=s;
+
+    while (*p != '\0')
+    {
+        printf("*p: %c\n", *p);
+        printf("p: %p\n", p);
+        p++;
+    }
+    return (p-s);
+}
+
 int main(void)
 {
-    
+    char *myStr = "hello!";
+    printf("The length of the string is: %d\n", myStrLen(myStr));
 
     return (0);
 }
