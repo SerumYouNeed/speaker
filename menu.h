e9e246a779ac092274967b0f1560a8fedc23c040
SerumYouNeed <greg.miler@gmail.com>
2026-04-29 18:17:05 +0200

    wersja todo

---
+++ menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+void printMenu(void);
+int selectTask(int a);
+void addTask(char *s, int taskCounter);
+int lineCounter(char *s);
+
+#endif // MENU_H   
--- temp.c
+++ temp.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include "menu.h"
 
-
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    FILE *fp;
+    if (argc != 2)
+    {
+        printf("Usage: %s <list_name>\n", argv[0]);
+        return 1;
+    }
 
-    fp = fopen("list.txt", "w");
-    char task[1024];
-    int taskNumber = 1;
-
-    printf("Enter new task: \n");
-    scanf(" %s", task);
-    fprintf(fp, "%d. %s", taskNumber, task);
-
+    char listName[101];
+    strcpy(listName, argv[1]);
+    strcat(listName, ".txt");
+    printMenu();
+    int task = selectTask(task);
+    switch (task)
+    {    
+        case 1: addTask(listName, lineCounter(listName));
+                break;
+        case 2: // deleteTask(listName);
+                break;
+        case 3: // markComplete(listName);
+                break;
+        case 4: // printList(listName);
+                break;
+        default: printf("Invalid option.\n");
+    }
+    
+    return (0);
 }
